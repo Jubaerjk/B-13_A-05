2514bb06bfe05c7d76a30fe0855117bdd2af1500
Jubaerjk <jubaer.khan099@gmail.com>
2026-03-07 09:37:21 +0600

    Login fixed

--- script/login.js
+++ script/login.js
@@ -1,11 +1,14 @@
-document.getElementById("login-btn").addEventListener("click", function () {
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-  if (username === "admin" && password === "admin123") {
-    alert("login successful");
-    window.location.href("home.html");
-  } else {
-    return;
-  }
-});
+    if (username === "admin" && password === "admin123") {
+      alert("login successful");
+      window.location.assign("home.html");
+    } else {
+      return;
+    }
+  });
