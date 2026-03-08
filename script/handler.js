800e63a92a5c170ed15ae24a25c374fbe997adc3
Jubaerjk <jubaer.khan099@gmail.com>
2026-03-09 03:01:06 +0600

    included search function with own logic

--- script/handler.js
+++ script/handler.js
@@ -1,26 +1,71 @@
 let allIssues = [];
-
-const loadIssues = (status = "all") => {
+const loadIssues = () => {
   fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
     .then((res) => res.json())
-    .then((data) => {
-      allIssues = data.data;
-      filterAndDisplay(status);
+    .then((json) => {
+      allIssues = json.data;
+      filterAndDisplay(json.data);
+      updateIssueCount("all");
     });
 };
 
-const filterAndDisplay = (status) => {
-  if (status === "all") {
-    displayIssues(allIssues);
-  } else {
-    const filtered = allIssues.filter((issue) => issue.status === status);
-    displayIssues(filtered);
-  }
+const filterAndDisplay = (issues) => {
+  displayIssues(issues);
+
+  const openIssues = issues.filter((issue) => issue.status === "open");
+  const closedIssues = issues.filter((issue) => issue.status === "closed");
 };
 
 const resetActiveNavigation = () => {
   const navigationBtns = document.querySelectorAll(".navigation-btn");
   navigationBtns.forEach((btn) => btn.classList.remove("active"));
+};
+
+const handleAllBtn = () => {
+  const allBtn = document.getElementById("all-btn");
+  allBtn.addEventListener("click", () => {
+    resetActiveNavigation();
+    allBtn.classList.add("active");
+    filterAndDisplay(allIssues);
+    updateIssueCount("all");
+  });
+};
+
+const handleOpenBtn = () => {
+  const openBtn = document.getElementById("open-btn");
+  openBtn.addEventListener("click", () => {
+    resetActiveNavigation();
+    openBtn.classList.add("active");
+    const openIssues = allIssues.filter((issue) => issue.status === "open");
+    filterAndDisplay(openIssues);
+    updateIssueCount("open");
+  });
+};
+
+const handleClosedBtn = () => {
+  const closedBtn = document.getElementById("closed-btn");
+  closedBtn.addEventListener("click", () => {
+    resetActiveNavigation();
+    closedBtn.classList.add("active");
+    const closedIssues = allIssues.filter((issue) => issue.status === "closed");
+    filterAndDisplay(closedIssues);
+    updateIssueCount("closed");
+  });
+};
+
+const updateIssueCount = (type) => {
+  const issueCount = document.getElementById("issue-count");
+  issueCount.innerHTML = "";
+
+  if (type === "all") {
+    issueCount.innerHTML = `${allIssues.length} Issues`;
+  } else if (type === "open") {
+    const openIssues = allIssues.filter((issue) => issue.status === "open");
+    issueCount.innerHTML = `${openIssues.length} Issues`;
+  } else if (type === "closed") {
+    const closedIssues = allIssues.filter((issue) => issue.status === "closed");
+    issueCount.innerHTML = `${closedIssues.length} Issues`;
+  }
 };
 
 const displayIssues = (data) => {
@@ -54,14 +99,14 @@ const displayIssues = (data) => {
   </p>
 
   <!-- Labels -->
-  <div class="flex justify-between items-center mb-3">
-    <a class="text-red-500 bg-red-100 py-1 px-4 rounded-full flex items-center gap-2">
+  <div class="flex gap-2 items-center mb-3 text-xs uppercase">
+    <a class="text-red-500 bg-red-100 border border-red-500 py-1 px-4 rounded-full flex items-center gap-2">
       <i class="fa-solid fa-bug"></i>
-      <span>BUG</span>
+      <span>${issue.labels[0]}</span>
     </a>
-    <a class="text-[#D97706] bg-[#D97706]/20 py-1 px-4 rounded-full flex items-center gap-2">
+    <a class="text-[#D97706] bg-[#FFF8DB] border border-[#D97706] py-1 px-4 rounded-full flex items-center gap-2">
       <i class="fa-regular fa-life-ring"></i>
-      <span>HELP WANTED</span>
+      <span>${issue.labels[1]}</span>
     </a>
   </div>
 
@@ -85,35 +130,9 @@ const displayIssues = (data) => {
   });
 };
 
-const handleAllBtn = () => {
-  const allBtn = document.getElementById("all-btn");
-  allBtn.addEventListener("click", () => {
-    resetActiveNavigation();
-    allBtn.classList.add("active");
-    filterAndDisplay("all");
-  });
-};
-
-const handleOpenBtn = () => {
-  const openBtn = document.getElementById("open-btn");
-  openBtn.addEventListener("click", () => {
-    resetActiveNavigation();
-    openBtn.classList.add("active");
-    filterAndDisplay("open");
-  });
-};
-
-const handleClosedBtn = () => {
-  const closedBtn = document.getElementById("closed-btn");
-  closedBtn.addEventListener("click", () => {
-    resetActiveNavigation();
-    closedBtn.classList.add("active");
-    filterAndDisplay("closed");
-  });
-};
-
 const showDetails = (index) => {
   const data = allIssues[index];
+  console.log(data);
   displayDetails(data);
 };
 
@@ -121,52 +140,100 @@ const displayDetails = (data) => {
   const modal = document.getElementById("issue_modal");
   const detailsContainer = document.getElementById("details-container");
   detailsContainer.innerHTML = `
-  <div class="card-body">
-    <h2 class="text-lg font-semibold text-[#1F2937]">
-      ${data.title}
-    </h2>
-    <p class="text-[#64748B]">
-      ${data.description}
-    </p>
-    <div class="flex justify-between items-center">
-      <button
-        class="text-red-500 bg-red-100 py-1 px-4 rounded-full font-medium"
-      >
-        <i class="fa-solid fa-bug"></i>
-        <span>BUG</span>
-      </button>
-      <button
-        class="text-[#D97706] bg-[#D97706]/20 py-1 px-4 rounded-full font-medium"
-      >
-        <i class="fa-regular fa-life-ring"></i>
-        <span>HELP WANTED</span>
-      </button>
-    </div>
-    <hr class="my-4 opacity-30" />
-    <div class="space-y-2">
-      <p>
-        Added By:
-        <span class="font-semibold text-[#1F2937] text-[16px]"
-          >${data.author}</span
-        >
-      </p>
-      <p>
-        Date:
-        <span class="font-semibold text-[#1F2937] text-[16px]"
-          >${data.createdAt}</span
-        >
-      </p>
+<div class="card-body">
+  <!-- Title -->
+  <h2 class="text-2xl font-bold text-gray-800">
+    ${data.title}
+  </h2>
+
+  <!-- Status + Meta -->
+  <div class="flex gap-2 items-center mt-2">
+    <button
+      class="px-2 py-1 rounded-full text-xs 
+      ${data.status === "open" ? "bg-green-500 text-white" : "bg-red-500 text-white"}"
+    >
+      ${data.status === "open" ? "Opened" : "Closed"}
+    </button>
+
+    <div class="flex gap-2 text-xs text-gray-500">
+      ${
+        data.status === "open"
+          ? `<p class="flex items-center gap-1">
+      <i class="fa-solid fa-circle text-[8px]"></i> 
+      Opened by ${data.assignee} 
+      </p>`
+          : ""
+      }
+   
+      ${
+        data.status === "open"
+          ? `
+          <p class="flex items-center gap-1">
+      <i class="fa-solid fa-circle text-[8px]"></i> 
+       ${data.updatedAt}
+      </p>`
+          : ""
+      }   
     </div>
   </div>
-  <div class="modal-action">
-      <form method="dialog">
-        <!-- if there is a button in form, it will close the modal -->
-        <button class="btn">Close</button>
-      </form>
+
+  <!-- Labels -->
+  <div class="flex gap-2 items-center my-4 text-xs uppercase">
+    <a class="text-red-500 bg-red-100 border border-red-500 py-1 px-4 rounded-full flex items-center gap-2">
+      <i class="fa-solid fa-bug"></i>
+      <span>${data.labels[0]}</span>
+    </a>
+    <a class="text-[#D97706] bg-[#FFF8DB] border border-[#D97706] py-1 px-4 rounded-full flex items-center gap-2">
+      <i class="fa-regular fa-life-ring"></i>
+      <span>${data.labels[1]}</span>
+    </a>
+  </div>
+
+  <!-- Description -->
+  <p class="text-gray-500 mb-4">
+    ${data.description}
+  </p>
+
+  <!-- Assignee + Priority -->
+  <div class="flex gap-30 bg-slate-100 p-4 rounded-lg">
+    <div class="space-y-1">
+      <p class="text-gray-500">Assignee:</p>
+      <p class="font-semibold text-gray-800 text-[16px] capitalize">
+        ${data.assignee}
+      </p>
     </div>
+    <div class="space-y-1">
+      <p class="text-gray-500">Priority:</p>
+      <a class="text-white bg-red-500 py-1 px-4 rounded-full uppercase text-xs">
+      ${data.priority || "High"}
+      </a>  
+    </div>
+  </div>
+
+  <!-- Modal Action -->
+  <div class="modal-action mt-4">
+    <form method="dialog">
+      <button class="btn btn-primary">Close</button>
+    </form>
+  </div>
+</div>
+
   `;
   modal.showModal();
 };
+
+const input = document.getElementById("input-search");
+const searchBtn = document.getElementById("btn-search");
+
+const handleSearch = () => {
+  const searchTerm = input.value.toLowerCase();
+  const filteredIssues = allIssues.filter((issue) =>
+    issue.title.toLowerCase().includes(searchTerm),
+  );
+  displayIssues(filteredIssues);
+};
+
+searchBtn.addEventListener("click", handleSearch);
 
 handleAllBtn();
 handleOpenBtn();
